8eb6c22463aa5d456274017362a806acc99a5f71
Jay Agrawal <jayagrawal321@gmail.com>
2020-04-30 16:32:45 -0400

    Initialize DRF

--- backend/peer_eval_api/peer_eval_api/settings.py
+++ backend/peer_eval_api/peer_eval_api/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'peer_eval',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permisions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
