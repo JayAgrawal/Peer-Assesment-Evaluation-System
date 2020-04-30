6b9839602e969d9a746a31b755859b4eb6806254
Jay Agrawal <jayagrawal321@gmail.com>
2020-04-30 17:28:22 -0400

    Adjust URL '/'

--- backend/peer_eval_api/peer_eval/urls.py
+++ backend/peer_eval_api/peer_eval/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/Student/', views.ListStudent.as_view()),
-    path('/Student/<int:pk>/', views.DetailStudent.as_view()),
+    path('Student/', views.ListStudent.as_view()),
+    path('Student/<int:pk>/', views.DetailStudent.as_view()),
 
-    path('/NumericEval/', views.ListNumericEvaluation.as_view()),
-    path('/NumericEval/<int:pk>/', views.DetailNumericEvaluation.as_view()),
+    path('NumericEval/', views.ListNumericEvaluation.as_view()),
+    path('NumericEval/<int:pk>/', views.DetailNumericEvaluation.as_view()),
 
-    path('/OpenEval/', views.ListOpenEndedEvaluation.as_view()),
-    path('/OpenEval/<int:pk>/', views.DetailOpenEndedEvaluation.as_view()),
+    path('OpenEval/', views.ListOpenEndedEvaluation.as_view()),
+    path('OpenEval/<int:pk>/', views.DetailOpenEndedEvaluation.as_view()),
 
 ]
