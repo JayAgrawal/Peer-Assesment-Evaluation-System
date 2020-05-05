774d346dd1f4a0dd8b1faa98aec824a2fb648096
Jay Agrawal <jayagrawal321@gmail.com>
2020-05-05 14:48:44 -0400

    Add Requirements for Server, Unique Email

--- backend/peer_eval_api/peer_eval/models.py
+++ backend/peer_eval_api/peer_eval/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Student(models.Model):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
-    email = models.CharField(max_length=30) 
+    email = models.CharField(max_length=30, unique=True) 
     password = models.CharField(max_length=30)
 
     def __str__(self):
