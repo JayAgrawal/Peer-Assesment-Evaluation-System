dcfc81c431d2312417b73d03afe936d5bae6f3c0
Jay Agrawal <jayagrawal321@gmail.com>
2020-05-07 19:05:52 -0400

    Added Team foreign key to Student

--- backend/peer_eval_api/peer_eval/models.py
+++ backend/peer_eval_api/peer_eval/models.py
@@ -2,20 +2,22 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
-class Student(models.Model):
-    firstName = models.CharField(max_length=30)
-    lastName = models.CharField(max_length=30)
-    email = models.CharField(max_length=30, unique=True) 
-    password = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.email
-
 class Team(models.Model):
     member1 = models.CharField(max_length=30)
     member2 = models.CharField(max_length=30)
     member3 = models.CharField(max_length=30)
     member4 = models.CharField(max_length=30)
+
+class Student(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    email = models.CharField(max_length=30, unique=True) 
+    password = models.CharField(max_length=30)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.email
+
 
 class Evaluation(models.Model):
     evaluation_name = models.CharField(max_length=40, default='Test Eval')
